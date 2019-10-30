cc7aa355523d5ab66cac1becc875540306ba05da
Bladimir Orellana <me@bladimirorellana.com>
2019-10-30 01:44:59 -0500

    Removing modules

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Bladimir y sonia
+          Learn React with
         </a>
       </header>
     </div>
