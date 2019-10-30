e44bb55aff3e5306716a1be466d20b690fac6a6f
Bladimir Orellana <me@bladimirorellana.com>
2019-10-30 01:35:22 -0500

    Updates

--- precache-manifest.3dd2d6291c545942a35b7d603fde4c03.js
+++ precache-manifest.3dd2d6291c545942a35b7d603fde4c03.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f067ddec14508820ec377832710f168",
+    "revision": "f4c3031d6120efffea112047c143e867",
     "url": "/find-me-a-book/index.html"
   },
   {
-    "revision": "4d95e0080daa9c3cefbf",
+    "revision": "335cf28e0ec49351673e",
     "url": "/find-me-a-book/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/find-me-a-book/static/js/2.22d15270.chunk.js"
   },
   {
-    "revision": "4d95e0080daa9c3cefbf",
-    "url": "/find-me-a-book/static/js/main.54f52256.chunk.js"
+    "revision": "335cf28e0ec49351673e",
+    "url": "/find-me-a-book/static/js/main.adb45b96.chunk.js"
   },
   {
     "revision": "e6bbf91999a9b92e4b62",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/find-me-a-book/precache-manifest.2e345cb96543961f9c5e0594eb57b352.js"
+  "/find-me-a-book/precache-manifest.3dd2d6291c545942a35b7d603fde4c03.js"
 );
 
 self.addEventListener('message', (event) => {
