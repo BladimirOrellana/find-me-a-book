70765af9e0aa0ef5bb4ccd1f3d2712a3b7ebfdd0
Bladimir Orellana <me@bladimirorellana.com>
2019-10-30 01:18:24 -0500

    Updates

---
+++ precache-manifest.13dcc1910591dd6f74ff37d4f4b6fb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49629f66965329654179a434bf3fc0d",
+    "url": "/find-me-a-book/index.html"
+  },
+  {
+    "revision": "8ae2835768f17c0a4e04",
+    "url": "/find-me-a-book/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "702bee8bcaba732595ef",
+    "url": "/find-me-a-book/static/js/2.22d15270.chunk.js"
+  },
+  {
+    "revision": "8ae2835768f17c0a4e04",
+    "url": "/find-me-a-book/static/js/main.5246274b.chunk.js"
+  },
+  {
+    "revision": "e6bbf91999a9b92e4b62",
+    "url": "/find-me-a-book/static/js/runtime-main.66a05b8f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/find-me-a-book/static/media/logo.25bf045c.svg"
+  }
+]);
